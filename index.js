db0c46bceb9ae102c3ad2fe1b1de308539cff845
Javier Salcedo <85317503+Javiersalcedoj@users.noreply.github.com>
2021-07-29 15:42:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const celeste = document.getElementById('celeste');
 const violeta = document.getElementById('violeta');
 const naranja = document.getElementById('naranja');
 const verde = document.getElementById('verde');
-const ULTIMO_NIVEL = 3;
+const ULTIMO_NIVEL = 10;
 
 class Juego {
     constructor () {
